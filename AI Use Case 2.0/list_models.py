8042bb26cd5b8e87fd258ae0d662a3b19a781280
jetang600 <jetang@iu.edu>
2025-10-16 12:33:12 -0400

    Delete AI Use Case 2.0/list_models.py

--- AI Use Case 2.0/list_models.py
+++
@@ -1,7 +0,0 @@
-import os, openai
-from dotenv import load_dotenv
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-models = openai.Model.list()
-print([m["id"] for m in models["data"]][:50])  # print first 50 ids
